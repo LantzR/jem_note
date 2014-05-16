b098ea14402b7faff75cf7d6ea21bf0bec9956f6
Lantz <LantzR@gMail.com>
2014-05-16 00:55:28 -0700

    Generater Jem model - with rspec and factory girl

---
+++ db/migrate/20140516074702_create_jems.rb
@@ -0,0 +1,13 @@
+class CreateJems < ActiveRecord::Migration
+  def change
+    create_table :jems do |t|
+      t.string :name
+      t.integer :seq, limit: 3
+      t.string :comment, limit: 40
+
+      t.timestamps
+    end
+    add_index :jems, :name, unique: true
+    add_index :jems, :seq
+  end
+end
---
+++ spec/factories/jems.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :jem do
+    name "MyString"
+    seq 1
+    comment "MyString"
+  end
+end

ab7b2ada308ea7f2652180c8c62b753926736121
Lantz <LantzR@gMail.com>
2014-05-26 16:21:20 -0700

    Almost working - missing index

--- db/migrate/20140516074702_create_jems.rb
+++ db/migrate/20140516074702_create_jems.rb
@@ -1,21 +1,20 @@
 class CreateJems < ActiveRecord::Migration
   def change
     create_table :jems, 
-    {:id          =>  false
+    {:id          =>  false,
      :primary_key =>  :name
     } do |t|
     
-      t.string :name, null: false
-      t.integer :seq, limit: 3
-      t.string :comment, limit: 40
+        t.string :name, :uniq, null: false
+        t.integer :seq, limit: 3
+        t.string :comment, limit: 40
 
-      t.timestamps
+        t.timestamps
     end
     add_index :jems, :seq
   end
   def up
-#    add_index :jems, :seq
-   # execute "alter table jems add primary key (id);"
+     execute "alter table jems add primary key (name);"
   end
   def down
 #    drop_index :jems, :seq
